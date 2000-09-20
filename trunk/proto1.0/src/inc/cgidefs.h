2af523656f0f6cbe7780e7dc9f17c5ecde0a1f24
akari <akari@8b103a1a-41bf-996e-811d-b05a68ea7469>
2000-09-20 18:26:37 +0000

    The Protomuck 1.00 Release

---
+++ trunk/proto1.0/src/inc/cgidefs.h
@@ -0,0 +1,17 @@
+#define SPACE       ' '
+#define TAB         '\t'
+#define LF          '\n'
+#define CR          '\r'
+#define PLUS        '+'
+#define PERCENT     '%'
+#define EQUALS      '='
+#define AMPERSAND   '&'
+#define HYPHEN      '-'
+#define UNDERSCORE  '_'
+#define ATSIGN      '@'
+#define SLASH       '/'
+
+#define SEPARATOR   "&"
+#define TRUE        1
+#define FALSE       0
+
