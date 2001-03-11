fc2c8beb01b5fdf3576d7cd0fa2ecebd83991b8f
akari <akari@8b103a1a-41bf-996e-811d-b05a68ea7469>
2001-03-11 04:05:22 +0000

    - Removed all of the old Neon MCP related stuff from the repository. - Removed version.c, since it's generated by the scripts anyway.

--- trunk/protomuck/src/inc/mcppkg.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MCPPKG_H
-#define _MCPPKG_H
-
-void show_mcp_error(McpFrame * mfr, char *topic, char *text);
-void mcppkg_simpleedit(McpFrame * mfr, McpMesg * msg, McpVer ver, void *context);
-
-#endif /* _MCPPKG_H */
-
-
