5261f7dda3784b809c49104f892305cede9d2a31
akari <akari@8b103a1a-41bf-996e-811d-b05a68ea7469>
2001-07-29 08:12:00 +0000

    - More work done to try and patch memory leaks and prevent ReCLEAR() errors.

--- protomuck/src/inc/config.h
+++ protomuck/src/inc/config.h
@@ -261,8 +261,8 @@
 
 /* This gives some debug malloc profiling, but also eats some overhead,
    so only define if your using it. */
-#undef MALLOC_PROFILING
-#undef CRT_DEBUG_ALSO TRUE
+#define MALLOC_PROFILING
+#define CRT_DEBUG_ALSO TRUE
 
 /************************************************************************/
 /************************************************************************/
--- protomuck/src/inc/version.h
+++ protomuck/src/inc/version.h
@@ -1,4 +1,4 @@
-#define PROTOBASE "1.70b5"
+#define PROTOBASE "1.70b7"
 #ifdef WIN32
   #define PROTOVER "Proto" PROTOBASE "-win32"
 #else
