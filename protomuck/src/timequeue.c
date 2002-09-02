fad10602940b318663146719b8cdba0007030fd5
akari <akari@8b103a1a-41bf-996e-811d-b05a68ea7469>
2002-09-02 19:43:44 +0000

    - Removed some debugging strings that slipped in there.

--- protomuck/src/timequeue.c
+++ protomuck/src/timequeue.c
@@ -976,7 +976,6 @@ dequeue_prog(dbref program, int sleeponly)
            has_refs(program, tqhead) || (tqhead->uid==program))
            && ((tqhead->fr) ? (!((tqhead->fr->multitask == BACKGROUND) &&
            (sleeponly == 2))) : (!sleeponly))) {
-notify_fmt(3166, "releasing tqhead.");
         ptr = tqhead;
         tqhead = tqhead->next;
         free_timenode(ptr);
@@ -1013,7 +1012,6 @@ notify_fmt(3166, "releasing tqhead.");
     for (ptr = tqhead; ptr; ptr = ptr->next) {
         if (ptr->typ == TQ_MUF_TYP && (ptr->subtyp == TQ_MUF_READ ||
              ptr->subtyp == TQ_MUF_TREAD)) {
-notify(3166, "Re-setting READ flag.");
             FLAGS(ptr->uid) |= (INTERACTIVE | READMODE);
         }
     }
