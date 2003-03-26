ba767d8ffea76e57a30eafbe0e55d3bf7dd2c37c
alynna <alynna@8b103a1a-41bf-996e-811d-b05a68ea7469>
2003-03-26 18:43:06 +0000

    1.80 beta 9.  Akari has declared thus no more major changes before release.

--- protomuck/src/inc/version.h
+++ protomuck/src/inc/version.h
@@ -1,7 +1,6 @@
-#define PROTOBASE "1.80b8"
+#define PROTOBASE "1.80b9"
   #define PROTOVER "Proto" PROTOBASE
 #define NEONVER "Neon2.17"
 #define VERSION "Muck2.2fb6.00b"
 
 extern const char *infotext[];
-
