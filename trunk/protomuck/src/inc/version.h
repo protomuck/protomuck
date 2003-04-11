07866412fa4fa9e8c0258e5820878c584e62f553
alynna <alynna@8b103a1a-41bf-996e-811d-b05a68ea7469>
2003-04-11 01:38:41 +0000

    Update version number to 1.80

--- trunk/protomuck/src/inc/version.h
+++ trunk/protomuck/src/inc/version.h
@@ -1,6 +1,6 @@
-#define PROTOBASE "1.80b9"
+#define PROTOBASE "1.80"
   #define PROTOVER "Proto" PROTOBASE
 #define NEONVER "Neon2.17"
-#define VERSION "Muck2.2fb6.00b"
+#define VERSION "Muck2.2fb6.00rc9"
 
 extern const char *infotext[];
