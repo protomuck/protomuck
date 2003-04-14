179d8995d8a6cd9f86f629a36e36a95bc3ddbddb
foxsteve <foxsteve@8b103a1a-41bf-996e-811d-b05a68ea7469>
2003-04-14 14:08:27 +0000

    Another fix to COPYPROPS. (Bug #720092)

--- protomuck/src/p_props.c
+++ protomuck/src/p_props.c
@@ -1495,10 +1495,10 @@ copy_props(dbref source, dbref destination, const char *sourcedir, const char *d
                     break; 
             }
             propcount++;
-        }
        
-        /* This has to go here, or it'll miss propdirs with values */
-        if (recurse) propcount += copy_props(source, destination, buf, buf2, recurse);
+            /* This has to go here, or it'll miss propdirs with values */
+            if (recurse) propcount += copy_props(source, destination, buf, buf2, recurse);
+        }
 
         /* find next property in current dir */ 
         propadr = next_prop(pptr, propadr, propname);
