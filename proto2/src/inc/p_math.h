66b643a6d2f8397c851a234b6bb99b10f9d42779
akari <akari@8b103a1a-41bf-996e-811d-b05a68ea7469>
2003-05-04 22:28:17 +0000

    Our start point for Proto2 (Was 1.81).

---
+++ proto2/src/inc/p_math.h
@@ -0,0 +1,47 @@
+extern void prim_add (PRIM_PROTOTYPE);
+extern void prim_subtract (PRIM_PROTOTYPE);
+extern void prim_multiply (PRIM_PROTOTYPE);
+extern void prim_divide (PRIM_PROTOTYPE);
+extern void prim_mod (PRIM_PROTOTYPE);
+extern void prim_bitor (PRIM_PROTOTYPE);
+extern void prim_bitxor (PRIM_PROTOTYPE);
+extern void prim_bitand (PRIM_PROTOTYPE);
+extern void prim_bitshift (PRIM_PROTOTYPE);
+extern void prim_and (PRIM_PROTOTYPE);
+extern void prim_or (PRIM_PROTOTYPE);
+extern void prim_not (PRIM_PROTOTYPE);
+extern void prim_lessthan (PRIM_PROTOTYPE);
+extern void prim_greathan (PRIM_PROTOTYPE);
+extern void prim_equal (PRIM_PROTOTYPE);
+extern void prim_lesseq (PRIM_PROTOTYPE);
+extern void prim_greateq (PRIM_PROTOTYPE);
+extern void prim_random (PRIM_PROTOTYPE);
+extern void prim_int(PRIM_PROTOTYPE);
+extern void prim_srand(PRIM_PROTOTYPE);
+extern void prim_getseed(PRIM_PROTOTYPE);
+extern void prim_setseed(PRIM_PROTOTYPE);
+extern void prim_plusplus(PRIM_PROTOTYPE);
+extern void prim_minusminus(PRIM_PROTOTYPE);
+extern void prim_dist2d(PRIM_PROTOTYPE);
+extern void prim_2ddist(PRIM_PROTOTYPE);
+extern void prim_3ddist(PRIM_PROTOTYPE);
+extern void prim_abs(PRIM_PROTOTYPE);
+extern void prim_sign(PRIM_PROTOTYPE);
+
+#define PRIMS_MATH_FUNCS prim_add, prim_subtract, prim_multiply, prim_divide, \
+    prim_mod, prim_bitor, prim_bitxor, prim_bitand, prim_bitshift, prim_and,  \
+    prim_or, prim_not, prim_lessthan, prim_greathan, prim_equal, prim_lesseq, \
+    prim_greateq, prim_random, prim_int, prim_srand, prim_setseed,            \
+    prim_getseed, prim_plusplus, prim_minusminus, prim_dist2d, prim_2ddist,   \
+    prim_3ddist, prim_abs, prim_sign
+
+#define PRIMS_MATH_NAMES  "+",  "-",  "*",  "/",          \
+    "%", "BITOR", "BITXOR", "BITAND", "BITSHIFT", "AND",  \
+    "OR",  "NOT",  "<",  ">",  "=",  "<=",                \
+    ">=", "RANDOM", "INT", "SRAND", "SETSEED", "GETSEED", \
+    "++", "--", "DIST2D", "2DDIST", "3DDIST", "ABS", "SIGN"
+ 
+#define PRIMS_MATH_CNT 29
+
+
+
---
+++ proto2/src/player.c
@@ -0,0 +1,187 @@
+#include "copyright.h"
+#include "config.h"
+
+#include "db.h"
+#include "params.h"
+#include "props.h"
+#include "tune.h"
+#include "interface.h"
+#include "externs.h"
+
+static hash_tab player_list[PLAYER_HASH_SIZE];
+
+dbref
+lookup_player(const char *name)
+{
+    hash_data *hd;
+
+    if ((hd = find_hash(name, player_list, PLAYER_HASH_SIZE)) == NULL)
+        return NOTHING;
+    else
+        return (hd->dbval);
+}
+
+dbref
+connect_player(const char *name, const char *password)
+{
+    dbref player;
+
+    if (*name == NUMBER_TOKEN && number(name + 1) && atoi(name + 1)) {
+        player = (dbref) atoi(name + 1);
+        if ((player < 0) || (player >= db_top)
+            || (Typeof(player) != TYPE_PLAYER))
+            player = NOTHING;
+    } else {
+        player = lookup_player(name);
+    }
+    if (player == NOTHING)
+        return NOTHING;
+    if (DBFETCH(player)->sp.player.password
+        && *DBFETCH(player)->sp.player.password
+        && strcmp(DBFETCH(player)->sp.player.password, password))
+        return NOTHING;
+
+    return player;
+}
+
+dbref
+create_player(const char *name, const char *password)
+{
+    char buf[80];
+    dbref player;
+
+    if (!ok_player_name(name) || !ok_password(password))
+        return NOTHING;
+    if (!tp_building || tp_db_readonly)
+        return NOTHING;
+
+    /* else he doesn't already exist, create him */
+    player = new_object();
+/*    newp = DBFETCH(player); */
+
+    /* initialize everything */
+    NAME(player) = alloc_string(name);
+    DBFETCH(player)->location = tp_player_start; /* home */
+    FLAGS(player) = TYPE_PLAYER;
+    if (valid_obj(tp_player_prototype)
+        && (Typeof(tp_player_prototype) == TYPE_PLAYER)) {
+        struct object *newp = DBFETCH(player);
+        struct plist *daprops = DBFETCH(player)->properties;
+
+        FLAGS(player) = FLAGS(tp_player_prototype);
+        FLAG2(player) = FLAG2(tp_player_prototype);
+
+        if (tp_pcreate_copy_props) {
+            daprops = (struct plist *) copy_prop(tp_player_prototype);
+            newp->properties = daprops;
+            newp->exits = NOTHING;
+            newp->contents = NOTHING;
+            newp->next = NOTHING;
+#ifdef DISKBASE
+            newp->propsfpos = 0;
+            newp->propsmode = PROPS_UNLOADED;
+            newp->propstime = 0;
+            newp->nextold = NOTHING;
+            newp->prevold = NOTHING;
+            dirtyprops(player);
+#endif
+        }
+        newp->location = tp_player_start;
+        DBDIRTY(player);
+    } else {
+        struct object *newp = DBFETCH(player);
+
+        newp->location = tp_player_start;
+        DBDIRTY(player);
+    }
+    OWNER(player) = player;
+    DBFETCH(player)->sp.player.home = tp_player_start;
+    DBFETCH(player)->exits = NOTHING;
+    DBFETCH(player)->sp.player.pennies = tp_start_pennies;
+    DBFETCH(player)->sp.player.password = alloc_string(password);
+    DBFETCH(player)->sp.player.curr_prog = NOTHING;
+    DBFETCH(player)->sp.player.insert_mode = 0;
+
+    /* link him to tp_player_start */
+    PUSH(player, DBFETCH(tp_player_start)->contents);
+    add_player(player);
+    DBDIRTY(player);
+    DBDIRTY(tp_player_start);
+    if (MLevel(player) > LM3)
+        SetMLevel(player, LM3);
+
+    sprintf(buf, CNOTE "%s is born!", PNAME(player));
+    anotify_except(DBFETCH(tp_player_start)->contents, NOTHING, buf, player);
+
+    return player;
+}
+
+void
+do_password(dbref player, const char *old, const char *newobj)
+{
+
+    if (Guest(player)) {
+        anotify_fmt(player, CFAIL "%s", tp_noguest_mesg);
+        return;
+    }
+
+    if (!DBFETCH(player)->sp.player.password
+        || strcmp(old, DBFETCH(player)->sp.player.password)) {
+        anotify_nolisten2(player,
+                          CFAIL "Syntax: @password <oldpass>=<newpass>");
+    } else if (!ok_password(newobj)) {
+        anotify_nolisten2(player, CFAIL "Bad new password.");
+    } else {
+        free((void *) DBFETCH(player)->sp.player.password);
+        DBSTORE(player, sp.player.password, alloc_string(newobj));
+        anotify_nolisten2(player, CFAIL "Password changed.");
+    }
+}
+
+void
+clear_players(void)
+{
+    kill_hash(player_list, PLAYER_HASH_SIZE, 0);
+    return;
+}
+
+void
+add_player(dbref who)
+{
+    hash_data hd;
+
+    hd.dbval = who;
+    if (add_hash(NAME(who), hd, player_list, PLAYER_HASH_SIZE) == NULL)
+        panic("Out of memory");
+    else
+        return;
+}
+
+
+void
+delete_player(dbref who)
+{
+    int result;
+
+    result = free_hash(NAME(who), player_list, PLAYER_HASH_SIZE);
+
+    if (result) {
+        int i;
+
+        wall_wizards(MARK
+                     "WARNING: Playername hashtable is inconsistent.  Rebuilding it.");
+        clear_players();
+        for (i = db_top; i-- > 0;) {
+            if (Typeof(i) == TYPE_PLAYER) {
+                add_player(i);
+            }
+        }
+        result = free_hash(NAME(who), player_list, PLAYER_HASH_SIZE);
+        if (result) {
+            wall_wizards(MARK
+                         "WARNING: Playername hashtable still inconsistent after rebuild.");
+        }
+    }
+
+    return;
+}
