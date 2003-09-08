5068f3ad16ab74dd15bf0ae435c4835365ea4ce0
foxsteve <foxsteve@8b103a1a-41bf-996e-811d-b05a68ea7469>
2003-09-08 23:12:37 +0000

    Added HTTPDATA ( i -- a ) prim. Takes a descr, returns array with HTTP  info in it.

--- trunk/proto2/src/inc/p_http.h
+++ trunk/proto2/src/inc/p_http.h
@@ -7,16 +7,17 @@ extern void prim_body_nextchar(PRIM_PROTOTYPE);
 extern void prim_body_prevchar(PRIM_PROTOTYPE);
 extern void prim_base64encode(PRIM_PROTOTYPE);
 extern void prim_base64decode(PRIM_PROTOTYPE);
+extern void prim_httpdata(PRIM_PROTOTYPE);
 
 #define PRIMS_HTTP_FUNCS prim_descr_safeboot, prim_descr_sendfile,    \
     prim_body_getchar, prim_body_nextchar, prim_body_prevchar,        \
-    prim_base64encode, prim_base64decode
+    prim_base64encode, prim_base64decode, prim_httpdata
 
 #define PRIMS_HTTP_NAMES "DESCR_SAFEBOOT", "DESCR_SENDFILE",          \
     "BODY_GETCHAR", "BODY_NEXTCHAR", "BODY_PREVCHAR", "BASE64ENCODE", \
-    "BASE64DECODE"
+    "BASE64DECODE", "HTTPDATA"
 
-#define PRIMS_HTTP_CNT 7
+#define PRIMS_HTTP_CNT 8
 
 #else /* !NEWHTTPD */
 #define PRIMS_HTTP_CNT 0
