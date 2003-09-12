e8f990456a3f67b0166f00e602426fb1069d9249
foxsteve <foxsteve@8b103a1a-41bf-996e-811d-b05a68ea7469>
2003-09-12 13:32:14 +0000

    Fixed the version number.

--- proto2/src/inc/version.h
+++ proto2/src/inc/version.h
@@ -1,13 +1,6 @@
-#ifdef BETA
 #define PROTOBASE "2.0b4"
 #define PROTOVER "Proto" PROTOBASE
 #define NEONVER "Neon2.17"
 #define VERSION "Muck2.2fb6.01"
-#else
-#define PROTOBASE "1.85"
-#define PROTOVER "Proto" PROTOBASE
-#define NEONVER "Neon2.17"
-#define VERSION "Muck2.2fb6.00"
-#endif
 
 extern const char *infotext[];
