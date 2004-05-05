d0ea6807fac5fbea7679628bd656d0dbb20d1523
foxsteve <foxsteve@8b103a1a-41bf-996e-811d-b05a68ea7469>
2004-05-05 16:22:58 +0000

    Fixed bug #936622 (array_getrange returns 1 extra element than exists)

--- trunk/proto2/src/array.c
+++ trunk/proto2/src/array.c
@@ -1151,7 +1151,7 @@ array_getrange(stk_array *arr, array_iter *start, array_iter *end)
         } else if (sidx > arr->items) {
             return NULL;
         }
-        if (eidx > arr->items) {
+        if (eidx >= arr->items) {
             eidx = arr->items - 1;
         } else if (eidx < 0) {
             return NULL;
