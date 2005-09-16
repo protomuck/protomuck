6c42ad77263c1384245cf72bf47122f77bca19b5
alynna <alynna@8b103a1a-41bf-996e-811d-b05a68ea7469>
2005-09-16 02:07:06 +0000

    No version number change.  Make log_wizards log wizzes set QUELL.

--- trunk/proto2/src/game.c
+++ trunk/proto2/src/game.c
@@ -606,12 +606,12 @@ process_command(int descr, dbref player, char *command)
         (
 	 (tp_log_commands || (tp_log_guests && Guest(OWNER(player)))) || 
          (tp_log_suspects && (FLAG2(OWNER(player)) & F2SUSPECT)) ||
-	 (tp_log_wizards && (Mage(OWNER(player))))
+	 (tp_log_wizards && (MLevel(OWNER(player)) >= LMAGE))
 	) &&
         (tp_log_interactive || !(FLAGS(player) & (INTERACTIVE | READMODE)))) {
         if (*command)           /* To prevent logging of NULL commands? FB6 change */
             log_command("%s%s%s%s(%d) in %s(%d):%s %s\n",
-                        Mage(OWNER(player)) ? "WIZ: " : (FLAG2(OWNER(player)) & F2SUSPECT) ? "SUSPECT: " : "",
+                        (MLevel(OWNER(player)) >= LMAGE) ? "WIZ: " : (FLAG2(OWNER(player)) & F2SUSPECT) ? "SUSPECT: " : "",
                         (Typeof(player) != TYPE_PLAYER) ? NAME(player) : "",
                         (Typeof(player) != TYPE_PLAYER) ? " by " : "",
                         NAME(OWNER(player)), (int) player,
