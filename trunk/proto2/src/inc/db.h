cd4c14752830e70a76200525a71c80b76af4e3c8
alynna <alynna@8b103a1a-41bf-996e-811d-b05a68ea7469>
2006-11-21 11:55:12 +0000

    MAX_COMMAND_SIZE 4k and BUFFER_LEN is now 16k to align to x86 boundaries. This is for the future streams support too.  This makes the max string size in proto 16384 bytes. Fixed a MAJOR crasher and possible buffer overflow 'sploit that was traced back to FB6, in MPI left, center, right.  Also fixed the strings associated with those commands. Fixed problems with program editing prims, specifically, program_deletelines. Fixed MD5HASH prim crasher, not detecting null strings and non-string types. Oops.

--- trunk/proto2/src/inc/db.h
+++ trunk/proto2/src/inc/db.h
@@ -34,7 +34,7 @@
 #endif
 
 /* max length of command argument to process_command */
-#define MAX_COMMAND_LEN 3072
+#define MAX_COMMAND_LEN 4096
 #define BUFFER_LEN ((MAX_COMMAND_LEN)*4)
 #define FILE_BUFSIZ ((BUFSIZ)*8)
 
