4ecc1d9a37d7a68df9f51540899add84025f922d
brevantes <brevantes@8b103a1a-41bf-996e-811d-b05a68ea7469>
2011-10-24 21:46:56 +0000

    p_regex.c:     * PRIM_REGSUB and PRIM_ARRAY_REGSUB now call pcre_study if REG_ALL is       specified by the user. PRIM_ARRAY_REGSUB will still call pcre_study if       an input array with more than two items is detected. inc/version.h:     * Version bump.

--- trunk/proto2/src/inc/version.h
+++ trunk/proto2/src/inc/version.h
@@ -1,6 +1,6 @@
 #define PROTOBASE "2.0b9.09.5"
 #define PROTOVER "Proto" PROTOBASE
 #define NEONVER "Neon2.17"
-#define VERSION "Muck2.2fb6.9084"
+#define VERSION "Muck2.2fb6.9085"
 
 extern const char *infotext[];
