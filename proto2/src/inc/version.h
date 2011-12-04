502f50398db74494a7a8fcea4eb514cbec6d564e
alynna <alynna@8b103a1a-41bf-996e-811d-b05a68ea7469>
2011-12-04 16:32:47 +0000

    bumped version # to 9.10.0 because array_fmtstrings and fmtstringEx represent a decent new feature.

--- proto2/src/inc/version.h
+++ proto2/src/inc/version.h
@@ -1,6 +1,6 @@
-#define PROTOBASE "2.0b9.09.7"
+#define PROTOBASE "2.0b9.10.0"
 #define PROTOVER "Proto" PROTOBASE
 #define NEONVER "Neon2.17"
-#define VERSION "Muck2.2fb6.9089"
+#define VERSION "Muck2.2fb6.91"
 
 extern const char *infotext[];
