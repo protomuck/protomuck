4ac1a3560b1d261f06e113bcd9c4598b8e920a45
davin <davin@protomuck.org>
2012-03-07 17:45:21 +0000

    interface.c, inc/interface.h: * Repeatedly poll a client for TTYPE fields until one is seen twice. Store the   resulting array in d->telopt.ttypes. d->telopt.termtype shares a string   pointer with the first array item to preserve compatibility. * MTTS detection. If any TTYPEs begin with "MTTS: ", store the integer value   that follows to d->telopt.mtts. Store 0 if the value is negative or trailing   non-integer data is detected. Default to 0. * New descriptor flags.
        DF_TELOPTS - Used to selectively enable TELOPT processing on descriptors.
                     This is necessary because descriptors assigned via /@ports
                     should not default to processing TELOPTS.
      DF_TTYPEDONE - Set when a TTYPE is seen twice. Used to to signify that any
                     further TTYPEs sent by the client are out-of-band.
    
    p_connects.c, inc/p_connects.h:
    * Added the new telopts to prim_getdescrinfo. 
    * CT_MUF descriptors assigned via /@ports will honor tp_ascii_descrs when
      converted to CT_MUCK.
    * Added a new primitive, DESCR_TELOPTS. This is used to enable TELOPT processing
      for descriptors allocated via @/ports. (replacement login screens)

--- ctmuf_notelopt/src/inc/p_connects.h
+++ ctmuf_notelopt/src/inc/p_connects.h
@@ -48,6 +48,7 @@ extern void prim_descr_sslp(PRIM_PROTOTYPE);
 extern void prim_descr_sendfile(PRIM_PROTOTYPE);
 extern void prim_descrtype(PRIM_PROTOTYPE);
 extern void prim_suid(PRIM_PROTOTYPE);
+extern void prim_descr_telopts(PRIM_PROTOTYPE);
 extern void prim_mccp_start(PRIM_PROTOTYPE);
 extern void prim_mccp_end(PRIM_PROTOTYPE);
 
@@ -67,7 +68,7 @@ extern void prim_mccp_end(PRIM_PROTOTYPE);
     prim_descrleastidle, prim_descrmostidle, prim_descrboot,                \
     prim_getdescrinfo, prim_descr_set, prim_descr_flagp, prim_bandwidth,    \
     prim_descrbufsize, prim_descr_sslp, prim_descr_sendfile, prim_suid,     \
-    prim_descrtype, prim_mccp_start, prim_mccp_end
+    prim_descrtype, prim_descr_telopts, prim_mccp_start, prim_mccp_end
 
 #define PRIMS_CONNECTS_NAMES "AWAKE?", "ONLINE", "CONCOUNT",  \
     "CONDBREF", "CONIDLE", "CONTIME", "CONHOST", "CONUSER",   \
@@ -82,7 +83,7 @@ extern void prim_mccp_end(PRIM_PROTOTYPE);
     "DESCRLEASTIDLE", "DESCRMOSTIDLE", "DESCRBOOT",           \
     "GETDESCRINFO", "DESCR_SET", "DESCR_FLAG?", "BANDWIDTH",  \
     "DESCRBUFSIZE", "DESCR_SSL?", "DESCR_SENDFILE", "SUID",   \
-    "DESCRTYPE", "MCCP_START", "MCCP_END"
+    "DESCRTYPE", "DESCR_TELOPTS", "MCCP_START", "MCCP_END"
 
-#define PRIMS_CONNECTS_CNT 52
+#define PRIMS_CONNECTS_CNT 53
 
