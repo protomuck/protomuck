77942daf629684339132594ea1b070ecf5d6213b
alynna <alynna@protomuck.org>
2012-05-27 14:11:25 +0000

    9.13.2 -- Incorporated READ/TREAD fix into my branch

--- src/inc/version.h
+++ src/inc/version.h
@@ -1,6 +1,6 @@
-#define PROTOBASE "2.0b9.13.1"
+#define PROTOBASE "2.0b9.13.2"
 #define PROTOVER "Proto" PROTOBASE
 #define NEONVER "Neon2.17"
-#define VERSION "Muck2.2fb6.91"
+#define VERSION "Muck2.2fb6.9132"
 #define BRANCH "alynna"
 extern const char *infotext[];
