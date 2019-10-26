d7885bf8aeb58af40acb5be3bddaf29f18df05d3
CyberLeo <cyberleo@cyberleo.net>
2019-10-25 23:42:38 -0500

    Tag branch

--- src/inc/version.h
+++ src/inc/version.h
@@ -2,5 +2,5 @@
 #define PROTOVER "Proto" PROTOBASE
 #define NEONVER "Neon2.17"
 #define VERSION "Muck2.2fb6.9134"
-/* #define BRANCH "alynna" */
+#define BRANCH "cyberleo"
 extern const char *infotext[];
